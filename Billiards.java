201a5b908699fd48936945e0e1f43313edf29d61
Andoni Vianez Ulloa <avu1002@alu.ubu.es>
2018-03-01 23:56:42 +0100

    Eliminado primer Todo: update with number of group label. See practice statement.

--- Billiards.java
+++ Billiards.java
@@ -18,9 +18,8 @@ public class Billiards extends JFrame {
 
 	private Board board;
 
-	// TODO update with number of group label. See practice statement.
 	private final int N_BALL = 2;
-	private Ball[] balls;
+	private Ball[] balls = new Ball[N_BALL];
 
 	public Billiards() {
 
